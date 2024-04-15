ad1557fdf887faf4c48b261e0112584d4116a9b7
Helge Baad Poulsen <hepou22@student.sdu.dk>
2024-04-15 10:56:52 +0200

    removed split module there was added in merge

--- Collisions/src/main/java/dk/sdu/mmmi/cbse/damage/damagePlugin.java
+++
@@ -1,4 +0,0 @@
-package dk.sdu.mmmi.cbse.damage;
-
-public class damagePlugin {
-}
--- Collisions/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-
-module DamageCounter {
-    requires Common;
-    requires javafx.graphics;
-    provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.damage.damageControlSystem;
-}
