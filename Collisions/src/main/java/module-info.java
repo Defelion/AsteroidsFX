47f6693752520ee89fc46572746d9e7af4480af1
Helge Baad Poulsen <hepou22@student.sdu.dk>
2024-05-12 21:56:35 +0200

    split virker havde lavet en fejl hved at kalde DamageCounter og Collisions modulerne det samme i modul-info

--- Collisions/src/main/java/module-info.java
+++ Collisions/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 import dk.sdu.mmmi.cbse.damagecol.damageControlSystem;
 
-module DamageCounter {
+module Collisions {
     requires Common;
     requires javafx.graphics;
     provides IPostEntityProcessingService with damageControlSystem;
