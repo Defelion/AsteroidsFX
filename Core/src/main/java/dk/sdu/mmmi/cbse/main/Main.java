7fc2f883ed167c8df2b77125110a26db418cfe57
Helge Baad Poulsen <hepou22@student.sdu.dk>
2024-05-14 00:33:32 +0200

    converted Core to spring

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -1,32 +1,9 @@
 package dk.sdu.mmmi.cbse.main;
 
-import dk.sdu.mmmi.cbse.common.data.Entity;
-import dk.sdu.mmmi.cbse.common.data.GameData;
-import dk.sdu.mmmi.cbse.common.data.GameKeys;
-import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Objects;
-import java.util.ServiceLoader;
-import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static java.util.stream.Collectors.toList;
 
@@ -34,13 +11,6 @@ import static java.util.stream.Collectors.toList;
  * The type Main.
  */
 public class Main extends Application {
-
-    private final GameData gameData = new GameData();
-    private final World world = new World();
-    private final Map<Entity, Polygon> polygons = new ConcurrentHashMap<>();
-    private final Pane gameWindow = new Pane();
-
-
     /**
      * The entry point of application.
      *
@@ -53,185 +23,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage window) throws Exception {
-        gameData.setDisplayHeight(800);
-        gameData.setDisplayWidth(800);
-        gameWindow.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
-        BackgroundFill bgFill = new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY);
-        Background bg = new Background(bgFill);
-        gameWindow.setBackground(bg);
-        gameWindow.autosize();
-        Pane menu = gameData.getMenu();
-        menu.setId("Menu");
-        menu.setPrefSize(gameData.getDisplayWidth(), gameData.getDisplayHeight());
-        menu.setTranslateX(0);
-        menu.setTranslateY(gameWindow.getHeight());
-        menu.setVisible(true);
-        menu.setDisable(false);
-        menu.autosize();
-        gameData.setMenu(menu);
-        gameWindow.getChildren().add(menu);
-        Scene scene = new Scene(gameWindow);
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode().equals(KeyCode.LEFT)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, true);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, true);
-            }
-            if (event.getCode().equals(KeyCode.UP)) {
-                gameData.getKeys().setKey(GameKeys.UP, true);
-            }
-            if (event.getCode().equals(KeyCode.E)) {
-                gameData.getKeys().setKey(GameKeys.E, true);
-            }
-            if (event.getCode().equals(KeyCode.A)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, true);
-            }
-            if (event.getCode().equals(KeyCode.D)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, true);
-            }
-            if (event.getCode().equals(KeyCode.W)) {
-                gameData.getKeys().setKey(GameKeys.UP, true);
-            }
-            if (event.getCode().equals(KeyCode.Q)) {
-                gameData.getKeys().setKey(GameKeys.Q, true);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, true);
-            }
-        });
-        scene.setOnKeyReleased(event -> {
-            if (event.getCode().equals(KeyCode.LEFT)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, false);
-            }
-            if (event.getCode().equals(KeyCode.RIGHT)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, false);
-            }
-            if (event.getCode().equals(KeyCode.UP)) {
-                gameData.getKeys().setKey(GameKeys.UP, false);
-            }
-            if (event.getCode().equals(KeyCode.E)) {
-                gameData.getKeys().setKey(GameKeys.E, false);
-            }
-            if (event.getCode().equals(KeyCode.Q)) {
-                gameData.getKeys().setKey(GameKeys.Q, false);
-            }
-            if (event.getCode().equals(KeyCode.A)) {
-                gameData.getKeys().setKey(GameKeys.LEFT, false);
-            }
-            if (event.getCode().equals(KeyCode.D)) {
-                gameData.getKeys().setKey(GameKeys.RIGHT, false);
-            }
-            if (event.getCode().equals(KeyCode.W)) {
-                gameData.getKeys().setKey(GameKeys.UP, false);
-            }
-            if (event.getCode().equals(KeyCode.SPACE)) {
-                gameData.getKeys().setKey(GameKeys.SPACE, false);
-            }
-        });
-
-        // Lookup all Game Plugins using ServiceLoader
-        for (IGamePluginService iGamePlugin : getPluginServices()) {
-            iGamePlugin.start(gameData, world);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AsteroidsServiceConfig.class);
+        for (String beanName : ctx.getBeanDefinitionNames()) {
+            System.out.println(beanName);
         }
-        for (Entity entity : world.getEntities()) {
-            Polygon polygon = new Polygon(entity.getPolygonCoordinates());
-            polygons.put(entity, polygon);
-            gameWindow.getChildren().add(polygon);
-        }
-
-        render();
-
-        window.setScene(scene);
-        window.setTitle("ASTEROIDS");
-        window.show();
-
-    }
-
-    private void render() {
-        new AnimationTimer() {
-            private long then = 0;
-
-            @Override
-            public void handle(long now) {
-                update();
-                draw();
-                gameData.getKeys().update();
-            }
-
-        }.start();
-    }
-
-    private void update() {
-
-        // Update
-        for (IEntityProcessingService entityProcessorService : getEntityProcessingServices()) {
-            entityProcessorService.process(gameData, world);
-        }
-         for (IPostEntityProcessingService postEntityProcessorService : getPostEntityProcessingServices()) {
-            postEntityProcessorService.process(gameData, world);
-        }
-    }
-
-    private void draw() {
-        Text text = new Text(10, 20,
-                "Destroyed asteroids: "+gameData.getDestroyedAsteroids()+
-                        "\nDestroyed enemies: "+gameData.getDestroyedEnemies()+
-                        "\nScore: "+gameData.getScore()
-                        +"\namount of elements: "+gameWindow.getChildren().size()
-        );
-        gameData.setDisplayWidth((int) gameWindow.getWidth());
-        /*gameData.setLog("");
-        gameData.setLog(gameData.getLog()+"\nData: "+gameData.getDisplayWidth()+", Window: "+gameWindow.getWidth());*/
-        gameData.setDisplayHeight((int) gameWindow.getHeight());
-        text.setFill(Color.WHITE);
-        if(gameData.getLog() != "") text.setText(text.getText()+"\nLog:"+gameData.getLog());
-        text.setId("text");
-        int count = 0;
-
-        for(Node node : gameWindow.getChildren()) {
-            if(Objects.equals(node.getId(), "text")) {
-                gameWindow.getChildren().remove(node);
-                gameWindow.getChildren().add(text);
-                count++;
-                break;
-            }
-            if(Objects.equals(node.getId(), "menu")) {
-                gameWindow.getChildren().remove(node);
-                gameWindow.getChildren().add(gameData.getMenu());
-            }
-        }
-        if(count == 0) gameWindow.getChildren().add(text);
-        for (Entity entity : world.getEntities()) {
-            Polygon polygon = polygons.get(entity);
-            if(entity.getDead()) {
-                world.removeEntity(entity);
-                gameWindow.getChildren().remove(polygon);
-                if(polygon != null) polygons.remove(polygon);
-                continue;
-            }
-            if (polygon == null) {
-                polygon = new Polygon(entity.getPolygonCoordinates());
-                polygons.put(entity,polygon);
-                gameWindow.getChildren().add(polygon);
-            }
-            polygon.setTranslateX(entity.getX());
-            polygon.setTranslateY(entity.getY());
-            polygon.setRotate(entity.getRotation());
-            polygon.setFill(entity.getPaint());
-            //polygon.accessibleTextProperty().setValue(String.valueOf(entity.getHealth()));
-        }
-    }
-
-    private Collection<? extends IGamePluginService> getPluginServices() {
-        return ServiceLoader.load(IGamePluginService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IEntityProcessingService> getEntityProcessingServices() {
-        return ServiceLoader.load(IEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
-    }
-
-    private Collection<? extends IPostEntityProcessingService> getPostEntityProcessingServices() {
-        return ServiceLoader.load(IPostEntityProcessingService.class).stream().map(ServiceLoader.Provider::get).collect(toList());
+        mainAsteroids mainAsteroids = ctx.getBean(mainAsteroids.class);
+        mainAsteroids.start(window);
+        mainAsteroids.render();
     }
 }
